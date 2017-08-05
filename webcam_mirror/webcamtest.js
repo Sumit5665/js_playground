81b775f16e5973562fa6e8612158911187dd8ab6
Sumit5665 <sumitpal547@gmail.com>
2017-08-06 00:40:19 +0530

    webcamtest

---
+++ webcam_mirror/webcamtest.js
@@ -0,0 +1,14 @@
+(function () {
+    navigator.getMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia);
+
+    navigator.getMedia(
+        {video:true, audio:false},
+        function (mediaStream) {
+            var video = document.getElementsByTagName('video')[0];
+            video.src = window.URL.createObjectURL(mediaStream);
+            video.play();
+        },   
+        function (error) {
+            console.log(error);
+        })   
+})();
